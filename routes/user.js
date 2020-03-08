28be63fdc82daae34edc5172c0aa884b3c2fc379
compmonk <zsescape@gmail.com>
2020-03-07 20:17:41 -0500

    Add User Transactions

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,11 @@ const MUUID = require('uuid-mongodb');
 const {isLoggedIn} = require("../core/login");
 const users = require("../data/users");
 const sessions = require("../data/sessions");
+const stellarService = require("../services/stellarService");
 
 const router = express.Router();
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded());
+// router.use(bodyParser.urlencoded());
 
 
 router.get("/:id", async (request, response) => {
@@ -20,10 +21,22 @@ router.get("/:id", async (request, response) => {
         response.status(e.http_code).send(e.message)
     }
 });
+
 router.put("/:id", async (request, response) => {
     try {
         const user = await users.updateUser(request.params.id, request.body, true);
         response.status(201).json(user);
+    } catch (e) {
+        response.setHeader('content-type', 'application/json');
+        response.status(e.http_code).send(e.message)
+    }
+});
+
+router.get("/:id/transactions", async (request, response) => {
+    try {
+        const user = await users.getUserById(request.params.id);
+        const transactions = await stellarService.getTransactions(user.publicKey);
+        response.send(transactions)
     } catch (e) {
         response.setHeader('content-type', 'application/json');
         response.status(e.http_code).send(e.message)
--- services/stellarService.js
+++ services/stellarService.js
@@ -1,13 +1,14 @@
 const StellarSdk = require('stellar-sdk');
 const axios = require('axios');
+const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
 
 
 async function createAccount() {
     const keyPair = StellarSdk.Keypair.random();
 
-    console.log(
-        keyPair.publicKey()
-    );
+    // console.log(
+    //     keyPair.publicKey()
+    // );
 
     const response = await axios.get(`https://friendbot.stellar.org/?addr=${keyPair.publicKey()}`);
     // console.log(response);
@@ -20,6 +21,44 @@ async function createAccount() {
     // transaction.sign(keypair)
 }
 
+async function transfer(sourceKeyPair, receiverKeyPair, amount) {
+    const sourcePublicKey = sourceKeyPair.publicKey();
+    const receiverPublicKey = receiverKeyPair.publicKey();
+    const account = await server.loadAccount(sourcePublicKey);
+    const fee = await server.fetchBaseFee();
+    const transaction = new StellarSdk.TransactionBuilder(account, {
+        fee,
+        networkPassphrase: StellarSdk.Networks.TESTNET
+    }).addOperation(StellarSdk.Operation.payment({
+        destination: receiverPublicKey,
+        asset: StellarSdk.Asset.native(),
+        amount: amount,
+    }))
+        .setTimeout(30)
+        .build();
+    transaction.sign(sourceKeyPair);
+}
+
+async function getTransactions(userPublicKeyPair) {
+    return server.transactions()
+        .forAccount(userPublicKeyPair)
+        .call()
+        .then(function (page) {
+            // console.log('Page 1: ');
+            // console.log(page.records);
+            return page.records;
+        })
+        // .then(function (page) {
+        //     console.log('Page 2: ');
+        //     console.log(page.records);
+        // })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
 module.exports = {
-    createAccount
+    createAccount,
+    transfer,
+    getTransactions
 };
