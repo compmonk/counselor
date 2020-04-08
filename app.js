866408070fc0f96237e05f822ccfb7f7c5d9e89f
Sanam Sritam Jena <sanamsritam@gmail.com>
2020-04-08 15:19:11 -0400

    Adding Redis

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
 app.use("/public", express.static(__dirname + "/public"));
-const path = require('path');
+const path = require("path");
 const configRoutes = require("./routes");
+const redis = require("redis");
+const client = redis.createClient();
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 configRoutes(app);
 app.get("/", (request, response, next) => {
-    response.sendFile(path.join(__dirname, "public", "html", "index.html"))
+  response.sendFile(path.join(__dirname, "public", "html", "index.html"));
 });
 app.listen(3000, () => {
-    console.log("We've now got a server!");
-    console.log("Your routes will be running on http://localhost:3000");
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
 });
