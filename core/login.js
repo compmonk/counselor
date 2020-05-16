1b09c553099de5f333d05591c4e068ac6f5c9477
compmonk <zsescape@gmail.com>
2020-05-16 00:11:02 -0400

    Clean Branch

--- core/login.js
+++
@@ -1,9 +0,0 @@
-const sessions = require("../data/sessions");
-
-const isLoggedIn = function (request) {
-    return !!(request.session.user && sessions.isSessionValid(request.sessionID));
-};
-
-module.exports = {
-    isLoggedIn
-};
