128b01af3760ed07afdeffeed96ff5dde8f872d1
compmonk <zsescape@gmail.com>
2020-05-18 11:05:53 -0400

    Add caching for currencies

--- src/server/data/currency.js
+++ src/server/data/currency.js
@@ -1,9 +1,18 @@
 const currencyModel = require("./models/currency");
 const mongoose = require("mongoose")
+const redisClient = require("../core/redisClient")
 
 async function getAllCurrencies() {
     try {
+        const idx = await redisClient.llenAsync("currencies");
+        if (idx !== 0) {
+            return await redisClient.lrangeAsync(
+                "currencies", 0, -1).map(JSON.parse);
+        }
         const currencies = await currencyModel.find({})
+        const multi = redisClient.multi()
+        currencies.map(JSON.stringify).map((currency) => multi.rpush("currencies", currency))
+        await multi.exec()
         return currencies
     } catch (e) {
         throw e
