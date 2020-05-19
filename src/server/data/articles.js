eae32450d11ac7b3fe22b44d99aeb2a90f0f351c
Sanam Sritam Jena <sanamsritam@gmail.com>
2020-05-19 02:38:03 -0400

    Removed redis implementation from articles

--- src/server/data/articles.js
+++ src/server/data/articles.js
@@ -314,33 +314,7 @@ async function getArticleByIdForUser(articleId, userId) {
     const permittedArticles = await userDao.getRecommendation(userId);
     for (var i = 0; i < permittedArticles.length; i++) {
       if (permittedArticles[i].toString() === articleId) {
-        let article_res;
-        const personIdx = await redisClient.hmgetAsync("ArticleIds", articleId);
-        if (personIdx[0] === null) {
-          article_res = await get(articleId);
-          if (article_res == null) {
-            throw "Article Id not found";
-          }
-          await redisClient.rpushAsync(
-            "ArticleList",
-            JSON.stringify(article_res)
-          );
-          const idx = await redisClient.llenAsync("ArticleList");
-          await redisClient.hmsetAsync(
-            "ArticleIds",
-            article_res._id.toString(),
-            idx
-          );
-        } else {
-          article_res = await redisClient.lrangeAsync(
-            "ArticleList",
-            personIdx[0] - 1,
-            personIdx[0] - 1
-          );
-          await redisClient.rpushAsync("ArticleList", article_res[0]);
-          article_res = JSON.parse(article_res[0]);
-        }
-
+        const article_res = await get(articleId);
         return article_res;
       }
     }
