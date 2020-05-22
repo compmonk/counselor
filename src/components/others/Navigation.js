33676985dfaced3c9f07f0e6d4571938974287ce
compmonk <zsescape@gmail.com>
2020-05-22 15:35:33 -0400

    Change NavBar style to right-align logged in user full name

--- src/components/others/Navigation.js
+++ src/components/others/Navigation.js
@@ -14,37 +14,33 @@ const Navigation = () => {
 }
 
 const UserNavigation = ({user}) => {
-    return (<div className="nav-bar">
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="/">
-                    Counselor
-                </Navbar.Brand>
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="/">
+                Counselor
+            </Navbar.Brand>
                 <Nav className="justify-content-end">
-                    <NavDropdown title="Article" id="nav-dropdown">
-                        <NavDropdown.Item href="/articles/new">New Article</NavDropdown.Item>
-                        <NavDropdown.Divider/>
-                        <NavDropdown.Item href="/articles/all">Library</NavDropdown.Item>
-                        <NavDropdown.Item href="/articles/recommendations">Recommendations</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title="Profile" id="nav-dropdown">
-                        <NavDropdown.Item href="/user/account">Account</NavDropdown.Item>
-                        <NavDropdown.Item href="/user/wallet">Wallet</NavDropdown.Item>
-                        <NavDropdown.Item href="/user/transactions">Transactions</NavDropdown.Item>
-                        <NavDropdown.Item href="/user/sessions">Sessions</NavDropdown.Item>
-                        <NavDropdown.Divider/>
-                        <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
-                    </NavDropdown>
-                    <Nav.Item>
-                        <Nav.Link href="/courses" title="Courses">Courses</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey="disabled" disabled>
-                            {user ? `${user.firstName} ${user.lastName}` : ""}
-                        </Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </Navbar>
-        </div>
+                <NavDropdown title="Article" id="nav-dropdown">
+                    <NavDropdown.Item href="/articles/new">New Article</NavDropdown.Item>
+                    <NavDropdown.Divider/>
+                    <NavDropdown.Item href="/articles/all">Library</NavDropdown.Item>
+                    <NavDropdown.Item href="/articles/recommendations">Recommendations</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Item>
+                    <Nav.Link href="/courses" title="Courses">Courses</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <Navbar.Collapse className="justify-content-end">
+                <NavDropdown title={user ? `${user.firstName} ${user.lastName}` : ""} id="nav-dropdown">
+                    <NavDropdown.Item href="/user/account">Account</NavDropdown.Item>
+                    <NavDropdown.Item href="/user/wallet">Wallet</NavDropdown.Item>
+                    <NavDropdown.Item href="/user/transactions">Transactions</NavDropdown.Item>
+                    <NavDropdown.Item href="/user/sessions">Sessions</NavDropdown.Item>
+                    <NavDropdown.Divider/>
+                    <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                </NavDropdown>
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
 
